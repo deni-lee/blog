f5f34b1ca6e257aa35fa7b4212d512ddd3905044
deni <deni851115@gamil.com>
2019-11-14 17:25:49 +0800

    跨域問題

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+export default {
+    mode: 'universal',
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#fff' },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+    ],
+    /*
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    axios: {
+        proxy: true
+    },
+    proxy: {
+        '/apis': {
+            // 测试环境
+            target: 'http://192.168.14.42:9994/', // 接口域名
+            changeOrigin: true, //是否跨域
+            pathRewrite: {
+                '^/apis': '' //需要rewrite重写的,
+            }
+        }
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
+    }
+}
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+
+  <button @click="aaa" >button</button>
+  
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  methods:{
+    aaa(){
+      axios.get('/apis/api/home/users')
+      .then((res)=>{
+        console.log(res)
+      }).catch((err)=>{
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
